1550d63883474db025ba5456c9a17c7e3c3cca89
Cihan <ylmz_cihan@outlook.com>
2022-04-16 19:18:34 +0300

    Added game over evaluation

--- script.js
+++ script.js
@@ -22,7 +22,6 @@ const gameBoard = (() => {
 
 })();
 
-
 const gameFlow = (() => {
     const playerX = Player('X');
     const playerO = Player('O');
@@ -43,7 +42,32 @@ const gameFlow = (() => {
             if(turn ? playerX.makeMove(tile) : playerO.makeMove(tile));
             turn = !turn;
             gameBoard.renderBoard();
+            checkGame(gameBoard.board);
         }
+    }
+
+    const checkGame = (board) => {
+        let isOver = false;
+        if (board[0] != '' && board[0] == board[1] && board[1] == board[2])
+            isOver = true;
+        else if (board[3] != '' && board[3] == board[4] && board[4] == board[5])
+            isOver = true;
+        else if (board[6] != '' && board[6] == board[7] && board[7] == board[8])
+            isOver = true;
+        else if (board[0] != '' && board[0] == board[4] && board[4] == board[8])
+            isOver = true;
+        else if (board[2] != '' && board[2] == board[4] && board[4] == board[6])
+            isOver = true;
+        else {
+            let tie = true;
+            board.forEach((tile) => {
+                if(tile === '')
+                    tie = false;
+            if (tie === true)
+                isOver = true;
+            })
+        }
+        console.log(isOver);
     }
     return {initBoard};
 })();
